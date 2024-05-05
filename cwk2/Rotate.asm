90c4b5c650885895ede417dc41c60d04dfbd071b
Planetlarg1 <146741451+Planetlarg1@users.noreply.github.com>
2024-05-05 14:21:15 +0000

    XOR

---
+++ cwk2/Rotate.asm
@@ -0,0 +1,44 @@
+// Rotate.asm
+// Rotates the bits of a 16-bit number left a specified amount of times
+
+@R3
+D=M      // Load original number into D
+@R4
+D=D+A    // Add number of rotations to original number
+@R4
+D=D-A    // Subtract original number from total (for loop counter)
+@R5
+M=D      // Store number of rotations in RAM[5] for loop counter
+
+(LOOP)
+@R4
+D=M      // Load loop counter into D
+@END_LOOP
+D;JEQ    // If D=0, exit loop
+@R3
+D=M      // Load number from RAM[3] into D
+@R5
+D=D<<1   // Shift left by 1 bit
+@R5
+M=D      // Store shifted number back in RAM[5]
+@R3
+D=M      // Load number from RAM[3] into D
+@32768   // Load 2^15 (MSB)
+D=A
+@R5
+D=D&A    // Check if MSB is set
+@END_LOOP
+D;JNE    // If MSB is set, jump to END_LOOP
+@1
+D=A
+@R5
+M=D      // Set LSB to 1
+(END_LOOP)
+@R4
+M=M-1    // Decrement loop counter
+@LOOP
+0;JMP    // Loop back to LOOP
+
+(END)
+@END
+0;JMP    // Infinite loop to halt the program
---
+++ cwk2/XOR.asm
@@ -0,0 +1,13 @@
+// XOR.asm
+// Performs a bitwise XOR between two values in RAM[3] and RAM[4], storing the result in RAM[5]
+
+@R3
+D=M      // Load RAM[3] into D
+@R4
+D=D^M    // XOR with RAM[4]
+@R5
+M=D      // Store result in RAM[5]
+
+(END)
+@END
+0;JMP    // Halt program
