54037e1a87d819cc932b81b6a5a726849ea67a79
Planetlarg1 <146741451+Planetlarg1@users.noreply.github.com>
2024-05-07 20:31:37 +0000

    Num

--- cwk2/XOR.asm
+++ cwk2/XOR.asm
@@ -1,12 +1,12 @@
 // XOR.asm
 // Performs a bitwise XOR between two values in RAM[3] and RAM[4], storing the result in RAM[5]
 
-@R3
+@3
 D=M      // Load RAM[3] into D
-@R4
+@4
 A=M
 D=D^M    // XOR with RAM[4]
-@R5
+@5
 M=D      // Store result in RAM[5]
 
 (END)
