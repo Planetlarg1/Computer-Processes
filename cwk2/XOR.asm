a8f1348f1142c52804ad9c3b0282c094bcadf048
Planetlarg1 <146741451+Planetlarg1@users.noreply.github.com>
2024-05-07 20:30:08 +0000

    A reg

--- cwk2/XOR.asm
+++ cwk2/XOR.asm
@@ -4,6 +4,7 @@
 @R3
 D=M      // Load RAM[3] into D
 @R4
+A=M
 D=D^M    // XOR with RAM[4]
 @R5
 M=D      // Store result in RAM[5]
@@ -11,5 +12,3 @@ M=D      // Store result in RAM[5]
 (END)
 @END
 D;JNE    // Halt program
-
-(END)
